9825f8139bb2e1bb4986d8f3ecedac35d2b704e0
Dtotoro27 <kurtromgamer27@outlook.es>
2017-03-03 11:57:28 +0100

    edit 1

--- RedBlueBox/Project1/main.cpp
+++ RedBlueBox/Project1/main.cpp
@@ -6,25 +6,16 @@
 #pragma comment( lib, "lib/SDL2main.lib")
 #pragma comment( lib, "lib/SDL2test.lib")
 
+
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Surface* window = NULL;
-	SDL_Surface* red = NULL;
-	SDL_Surface* blue = NULL;
 
-	window = SDL_CreateWindow("Box", SDL_WINDOW_CENTERED, SDL_WINDOW_CENTERED, 640, 450);
+	SDL_Init(SDL_INIT_EVERYTHING); //initialize all SDL funtions
 
-	red = SDL_LoadBMP("rojo.bmp");
-
-	SDL_BlitSurface(red, NULL, window, NULL);
-
-	SDL_UpdateWindowSurface(window);
-	SDL_FreeSurface(window);
-	SDL_FreeSurface(red);
-	SDL_FreeSurface(blue);
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
 
-	SDL_ShowWindow();
+	
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
